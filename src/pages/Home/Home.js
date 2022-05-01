f55fb68b7bed1ea003812705b1142d33b5651010
salmakhatun12345e <ummesalma0056@gmail.com>
2022-05-01 09:59:17 +0600

    navbar and banner started

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <div className='bg-primary banner-container'>
+                <div className='banner'>
+                    <h2>this is home</h2>
+                    <img src="https://i.ibb.co/FmDszk2/welcome-dental-care-vector-illustration-cartoon-character-533406-114-2.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
